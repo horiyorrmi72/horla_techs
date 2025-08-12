ae02cbddf627b08c72c559e10e4d760b091a54c4
yusuf <horiyorrmi72@gmail.com>
2025-08-12 14:15:15 +0100

    chores(path[router]):use navigation in place of router

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Send } from 'lucide-react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Contact() {
 	const [form, setForm] = useState({ name: '', email: '', message: '' });
