24f82559e708c7a7f4c2dbc6903f2eb194ae87a5
yusuf <horiyorrmi72@gmail.com>
2025-10-28 01:27:16 +0100

    chores(testimonials):clean-up testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,25 +1,39 @@
 export function Testimonials() {
 	const testimonials = [
 		{
-			name: '',
+			name: 'Ibrahim Agbesinga',
 			role: '',
 			quote:
 				'HorlaTech completely transformed our online presence. The website is fast, modern, and user-friendly. Our conversions have doubled since launch!',
 			image: '/images/testimonials/john.jpg',
 		},
 		{
-			name: '',
+			name: 'ODULATE JOSEPH',
+			role: '',
+			quote:
+				'Horlatechs is an outstanding tech company that consistently delivers excellence.For anyone looking for a dependable and forward-thinking tech partner, Horlatechs is definitely a company you can trust.',
+			image: '/images/testimonials/okafor.jpg',
+		},
+		{
+			name: 'Yusbyte Technologies',
 			role: '',
 			quote:
 				'The team delivered exactly what we envisioned — and more! Their attention to detail and post-launch support is top-notch.',
 			image: '/images/testimonials/okafor.jpg',
 		},
 		{
-			name: '',
+			name: 'George Onunkwor',
 			role: '',
 			quote:
 				'From design to deployment, HorlaTech exceeded expectations. Communication was smooth, and deadlines were met without issues.',
 			image: '/images/testimonials/david.jpg',
+		},
+		{
+			name: 'Azeez Ariyo',
+			role: '',
+			quote:
+				'Working with horlatechs has been a fantastic experience. Their team truly understands both the technical and business side of software development. From planning to delivery, they were reliable, responsive, and incredibly professional. They don’t just build solutions, they help you bring your vision to life with innovation and precision. I highly recommend them to anyone looking for a trustworthy and talented tech partner.',
+			image: '',
 		},
 	];
 
@@ -36,14 +50,20 @@ export function Testimonials() {
 							key={index}
 							className='bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg transition-shadow'
 						>
-							<div className='flex items-center mb-4'>
-								<img
+							<div className='flex items-center mb-4 rounded-full font-bold text-black bg-gradient-to-r from-gray-50 to-gray-500 w-15 h-15 text-center justify-center'>
+								{t.name
+									? t.name
+											.split(' ')
+											.map((n) => n.charAt(0).toUpperCase())
+											.join('')
+									: 'Client'}
+								{/* <img
 									src={t.image}
 									alt={t.name}
 									className='w-12 h-12 rounded-full object-cover mr-4'
-								/>
+								/> */}
 								<div>
-									<h4 className='font-semibold'>{t.name}</h4>
+									{/* <h4 className='font-semibold'>{t.name}</h4> */}
 									<p className='text-sm text-gray-500'>{t.role}</p>
 								</div>
 							</div>
