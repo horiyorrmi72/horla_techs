2ecdc0ed6f9f248a1bba39c3e5fb5f73147fe584
yusuf <horiyorrmi72@gmail.com>
2025-11-01 08:18:05 +0100

    feat(projects):projects list

---
+++ src/lib/projects.js
@@ -0,0 +1,57 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Post Postpartum Website",
+        slug: "5starmums",
+        description:
+            "Engineering a backend system that powers a postpartum support platform.",
+        category: "Engineering",
+        year: 2022 / 23,
+        image: "/assets/5starmums.jpg",
+        highlight: ""
+    },
+    {
+        id: 2,
+        title: "Custom Suprise Call App",
+        slug: "elder-app",
+        description:
+            "Engineering a custom suprise call mobile application that could place calls to PSTN (public switched telephone network) to celebrate called party and play selected song to the called party before connecting them to continue their conversation.",
+        category: "Engineering",
+        year: 2023 / 24,
+        image: "/assets/elder-call-app.jpg",
+        highlight: ""
+    },
+    {
+        id: 3,
+        title: "NGO Website",
+        slug: "women-safe-house",
+        description:
+            "Revamped a legacy NGO platform, increasing the efficiency of the site, making it easier for people to donate and engage in the course of the organization's mission.",
+        category: "Engineering",
+        year: 2025,
+        image: "/assets/women-safe-house.png",
+        highlight: "32% conversion uplift"
+    },
+    {
+        id: 4,
+        title: "Digital Marketing Agency",
+        slug: "digital-flex",
+        description:
+            "Product redesign for digital flex that reduced time-to-onboard by 45% and improved trial-to-paid conversion.",
+        category: "Product",
+        year: 2025,
+        image: "/assets/digital-flex.png",
+        highlight: "45% faster onboarding"
+    },
+    {
+        id: 5,
+        title: "Custom CRM",
+        slug: "acres-crm",
+        description:
+            "Custom CRM for a real estate agency in dubai, with AI-powered lead qualification and automated follow-ups.The AI handles calling the leads and qualifies lead based on the conversation, schedule appointment for inspection of property and also sends a reminder.",
+        category: "Engineering",
+        year: 2024,
+        image: "/assets/acrescrm.png",
+        highlight: "40% increase in lead conversion."
+    }
+];
