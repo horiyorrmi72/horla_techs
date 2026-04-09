dfe0777059947cd3f5aaada954eccfa53b4c0c98
yusuf ridwan <horiyorrmi72@gmail.com>
2026-04-09 07:27:17 +0100

    chores:add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://horlatechs.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 5000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+};
+
+export default config;
